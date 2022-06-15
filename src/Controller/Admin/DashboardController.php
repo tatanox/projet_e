8cf0ece6a4045573b6bd88ca7b9ce7b13cf36040
tatanox <95684648+tatanox@users.noreply.github.com>
2022-06-15 02:31:02 +0200

    user-end

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        // return parent::index();
+        return $this->render('admin/index.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Projet E');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateur', 'fa fa-user', User::class);
+    }
+}
